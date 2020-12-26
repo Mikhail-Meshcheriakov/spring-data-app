ae5b2a3511f397600eb3d55c482a26231568d372
Meshcheriakov-Mikhail <sekesh@mail.ru>
2020-12-26 16:59:43 +0700

    init commit

---
+++ src/main/java/ru/geekbrains/spring/data/SpringDataAppApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAppApplication.class, args);
+	}
+
+}
